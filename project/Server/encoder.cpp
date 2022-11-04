53b7857eea6387f0fe5ef7aeaeb1e7c28117401d
Tejas Lalit <tejasl@big04.seas.upenn.edu>
2022-11-04 02:24:41 -0400

    All Placeholders implemented, code working

--- project/Server/encoder.cpp
+++ project/Server/encoder.cpp
@@ -13,14 +13,32 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include "stopwatch.h"
+#include <math.h>
+#include <vector>
+#include <bits/stdc++.h> 
 
 #define NUM_PACKETS 8
 #define pipe_depth 4
 #define DONE_BIT_L (1 << 7)
 #define DONE_BIT_H (1 << 15)
+#define WIN_SIZE 16
+#define MAX_CHUNK_SIZE 8*1024 // 8KB max chunk size
+#define MIN_CHUNK_SIZE 16 // 16 bytes min chunk size
+#define TARGET 0
+#define PRIME 3
+#define MODULUS 256
 
 int offset = 0;
 unsigned char* file;
+
+/* data type for chunk */
+typedef struct chunk{
+	unsigned char *start;
+	unsigned int length;
+	uint64_t SHA_signature; 
+	uint32_t number; 
+} chunk_t;
+
 
 void handle_input(int argc, char* argv[], int* blocksize) {
 	int x;
@@ -39,6 +57,119 @@ void handle_input(int argc, char* argv[], int* blocksize) {
 	}
 }
 
+
+// CDC
+	// For calculating the first hash
+uint64_t hash_func(unsigned char *input, unsigned int pos)
+{
+	// put your hash function implementation here
+	uint64_t hash =0; 
+	for ( int i = 0 ; i<WIN_SIZE ; i++)
+	{
+		hash += (input[pos + WIN_SIZE-1-i]) * (pow(PRIME,i+1)); 
+	}
+	return hash; 
+}
+
+	//For rolling hash 
+void cdc_new(unsigned char *buff, chunk_t *chunk)
+{
+	static unsigned char *chunkStart = buff;
+
+	uint64_t hash = hash_func(buff,WIN_SIZE); 
+
+	if((hash % MODULUS) == TARGET)
+		{
+			chunk->length = MIN_CHUNK_SIZE; // minimum chunk size
+		}
+
+	//for (int i = WIN_SIZE +1 ; i < buff_size - WIN_SIZE ; i++)
+	for (int i = MIN_CHUNK_SIZE +1 ; i < MAX_CHUNK_SIZE - MIN_CHUNK_SIZE ; i++)
+	{
+		hash = (hash * PRIME - (buff[i-1])*pow(PRIME,WIN_SIZE+1) + (buff[i-1+WIN_SIZE]*PRIME)); 
+
+		if((hash % MODULUS) == TARGET)
+		{
+			chunk->length = i;
+			chunk->start = chunkStart;
+			chunkStart += i; // change the chunk start address to next start address;
+			return;
+		}
+	}
+	chunk->length = MAX_CHUNK_SIZE;
+	chunk->start = chunkStart;
+	chunkStart += MAX_CHUNK_SIZE; // change the chunk start address to next start address;
+	return;
+
+}
+
+uint64_t SHA_dummy(chunk_t *chunk)
+{
+	// put your hash function implementation here
+	uint64_t hash = 0x2890032189F32 ; 
+	
+	for ( unsigned int i = 0 ; i<WIN_SIZE ; i++)
+	{
+		hash += (chunk->start[WIN_SIZE + WIN_SIZE-1-i]) * (pow(PRIME,i+1)); 
+	}
+
+	for(unsigned int i=WIN_SIZE; i<(chunk->length-WIN_SIZE); i++){
+		hash = (hash * PRIME - (chunk->start[i-1])*pow(PRIME,WIN_SIZE+1) + (chunk->start[i-1+WIN_SIZE]*PRIME));
+	}
+	return hash;
+}
+
+int dedup(uint64_t SHA_result,chunk_t chunk)
+{
+	static std::vector<chunk_t> SHA_table;
+
+	for(unsigned int i=0; i<SHA_table.size();i++){
+		if(SHA_table[i].SHA_signature == SHA_result){
+			return SHA_table[i].number;
+		}
+	}
+	chunk.SHA_signature = SHA_result;
+	SHA_table.push_back(chunk);
+
+	return 0;
+}
+
+std::vector<int> LZW_encoding(chunk_t* chunk)
+{
+    std::cout << "Encoding\n";
+    std::unordered_map<std::string, int> table;
+    for (int i = 0; i <= 255; i++) {
+    	std::string ch = "";
+        ch += char(i);
+        table[ch] = i;
+    }
+ 
+    std::string p = "", c = "";
+    p += chunk->start[0];
+    int code = 256;
+    std::vector<int> output_code;
+    std::cout << "String\tOutput_Code\tAddition\n";
+    for (unsigned int i = 0; i < chunk->length; i++) {
+        if (i != chunk->length - 1)
+            c += chunk->start[i + 1];
+        if (table.find(p + c) != table.end()) {
+            p = p + c;
+        }
+        else {
+            std::cout << p << "\t" << table[p] << "\t\t"
+                 << p + c << "\t" << code << std::endl;
+            output_code.push_back(table[p]);
+            table[p + c] = code;
+            code++;
+            p = c;
+        }
+        c = "";
+    }
+    std::cout << p << "\t" << table[p] << std::endl;
+    output_code.push_back(table[p]);
+    return output_code;
+}
+
 int main(int argc, char* argv[]) {
 	stopwatch ethernet_timer;
 	unsigned char* input[NUM_PACKETS];
@@ -47,6 +178,9 @@ int main(int argc, char* argv[]) {
 	int length = 0;
 	int count = 0;
 	ESE532_Server server;
+	chunk_t cdc_chunk; 
+	int num_chunks = 0; 
+
 
 	// default is 2k
 	int blocksize = BLOCKSIZE;
@@ -112,7 +246,31 @@ int main(int argc, char* argv[]) {
 		length = buffer[0] | (buffer[1] << 8);
 		length &= ~DONE_BIT_H;
 		//printf("length: %d offset %d\n",length,offset);
-		memcpy(&file[offset], &buffer[HEADER], length);
+		//memcpy(&file[offset], &buffer[HEADER], length);
+		
+		printf("Start of Loop, packet size = %d\r\n",length);
+
+		for(int i=0; i<length; i += cdc_chunk.length){
+
+			cdc_new(&buffer[HEADER + i],&cdc_chunk);
+			num_chunks++;
+			cdc_chunk.number = num_chunks;
+
+			printf("Chunk Start = %p, length = %d\n",cdc_chunk.start, cdc_chunk.length);
+
+			uint64_t SHA_result = SHA_dummy(&cdc_chunk);
+
+			printf("SHA result = %ld, Chunk Number =%d\n", SHA_result, cdc_chunk.number);
+			
+			int chunk_num;
+			if((chunk_num = dedup(SHA_result,cdc_chunk))){
+				printf("DUPLICATE CHUNK : %p CHUNK NO : %d\n",cdc_chunk.start, chunk_num);
+
+			}else{
+				printf("NEW CHUNK : %p\n",cdc_chunk.start);
+				// LZW_encoding(&cdc_chunk);
+			}
+		}
 
 		offset += length;
 		writer++;
