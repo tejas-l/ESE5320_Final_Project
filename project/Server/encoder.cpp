1f26de43bac6e92e602f9b5f353640f3a8cdf0c3
Ameya Phadke <aphadke@big05.seas.upenn.edu>
2022-11-14 17:12:49 -0500

    changed units of output prints from ns to ms and changed latency to avg latency

--- project/Server/encoder.cpp
+++ project/Server/encoder.cpp
@@ -208,13 +208,13 @@ int main(int argc, char* argv[]) {
     // write file to root and you can use diff tool on board
     FILE *outfd = fopen(filename, "wb");
     int bytes_written = fwrite(&file[0], 1, offset, outfd);
-    std::cout << "Total time for CDC = " << cdc_time.avg_latency() << "ns" << std::endl;
-    std::cout << "Total time for SHA = " << sha_time.avg_latency() << "ns" << std::endl;
-    std::cout << "Total time for Dedup = " << dedup_time.avg_latency() << "ns" << std::endl;
-    std::cout << "Total time for LZW Encoding = " << lzw_time.avg_latency() << "ns" << std::endl;
-    std::cout << "Total time for Compress = " << compress_time.avg_latency() << "ns" << std::endl;
+    std::cout << "Average time for CDC = " << cdc_time.avg_latency() << "ms" << std::endl;
+    std::cout << "Average time for SHA = " << sha_time.avg_latency() << "ms" << std::endl;
+    std::cout << "Average time for Dedup = " << dedup_time.avg_latency() << "ms" << std::endl;
+    std::cout << "Average time for LZW Encoding = " << lzw_time.avg_latency() << "ms" << std::endl;
+    std::cout << "Average time for Compress = " << compress_time.avg_latency() << "ms" << std::endl;
 
-    std::cout << "Total runtime = " << total_time.latency() << "ns" << std::endl;
+    std::cout << "Total runtime = " << total_time.latency() << "ms" << std::endl;
     float output_time = (total_time.latency()/1000.0);
     std::cout << "Throughput = " << (file_size*8/1000000.0)/output_time << " Mb/s" << std::endl;
 
