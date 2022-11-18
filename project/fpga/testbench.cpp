e1df6144eb6f5748ba2ea6ab8f9ba5ab02763398
Pranav Deepak Khanolkar <pranavdk@big18.seas.upenn.edu>
2022-11-18 03:23:04 -0500

    Modified and working testbench for LZW and bit packing

--- project/fpga/testbench.cpp
+++ project/fpga/testbench.cpp
@@ -1,77 +1,15 @@
 #include <bits/stdc++.h>
 #include "LZW_HW.h"
 
-
-using namespace std;
-
-/* defines for compress function */
 #define OUT_SIZE_BITS 8
 #define MIN(A,B) ( (A)<(B) ? (A) : (B) );
 #define CODE_LENGTH 13
 
+using namespace std;
 
-
-std::vector<unsigned char> compress_test(std::vector<int> &compressed_data)
+vector<int> LZW_SW(string s1)
 {
-    std::vector<unsigned char> output_vector;
-    uint64_t Length = 0;
-    unsigned char Byte = 0;
-
-    uint8_t rem_inBytes = CODE_LENGTH;
-    uint8_t rem_outBytes = OUT_SIZE_BITS;
-
-
-    // for(int i=0; i<10;i++){
-    //     printf("Index = %d SW lzw res %d\n",i,compressed_data[i]);
-    // }
-
-    // for(int i=10; i>0;i--){
-    //     printf("Index = %d SW lzw res %d\n",compressed_data.size()-i,compressed_data[compressed_data.size()-i]);
-    // }
-
-    printf("compressed_data.size() = %d\n",compressed_data.size());
-    for(int i=0; i<compressed_data.size(); i++){
-        int inData = compressed_data[i];
-        rem_inBytes = CODE_LENGTH;
-
-        while(rem_inBytes){
-
-            int read_bits = MIN(rem_inBytes,rem_outBytes);
-            Byte = (Byte << read_bits) | ( inData >> (rem_inBytes - read_bits) );
-
-            rem_inBytes -= read_bits;
-            rem_outBytes -= read_bits;
-            // if(i>compressed_data.size()-10){
-            //     printf("\nSW Length loop = %d\n",Length);
-            // }
-            Length += read_bits;
-            inData &= ((0x1 << rem_inBytes) - 1);
-
-            if(rem_outBytes == 0){
-                //file[offset + Length/8 - 1] = Byte;
-                output_vector.push_back(Byte);
-                Byte = 0;
-                rem_outBytes = OUT_SIZE_BITS;
-            }
-        }
-    }
-
-    printf("SW length = %d\n",Length);
-    if(Length % 8 > 0){
-        Byte = Byte << (8 - (Length%8));
-        Length += (8 - (Length%8));
-        //file[offset + Length/8 - 1] = Byte;
-        output_vector.push_back(Byte);
-    }
-
-    printf("SW length = %d\n",Length);
-
-    return output_vector; // return number of bytes to be written to output file
-}
-
-
-vector<int> encoding(string s1)
-{
+    cout << "Encoding\n";
     unordered_map<string, int> table;
     for (int i = 0; i <= 255; i++) {
         string ch = "";
@@ -83,7 +21,7 @@ vector<int> encoding(string s1)
     p += s1[0];
     int code = 256;
     vector<int> output_code;
-    //cout << "String\tOutput_Code\tAddition\n";
+    cout << "String\tOutput_Code\tAddition\n";
     for (int i = 0; i < s1.length(); i++) {
         if (i != s1.length() - 1)
             c += s1[i + 1];
@@ -91,8 +29,8 @@ vector<int> encoding(string s1)
             p = p + c;
         }
         else {
-            //cout << p << "\t" << table[p] << "\t\t"
-            //     << p + c << "\t" << code << endl;
+            cout << p << "\t" << table[p] << "\t\t"
+                 << p + c << "\t" << code << endl;
             output_code.push_back(table[p]);
             table[p + c] = code;
             code++;
@@ -100,75 +38,88 @@ vector<int> encoding(string s1)
         }
         c = "";
     }
+    cout << p << "\t" << table[p] << endl;
     output_code.push_back(table[p]);
     return output_code;
 }
 
+std::vector<unsigned char> compress(std::vector<int> &compressed_data)
+{
+    uint64_t Length = 0;
+    unsigned char Byte = 0;
+    std::vector<unsigned char> output_vector; 
 
-int main()
-{    
-    
-    //unsigned char input[300] = "The Little Prince Chapter I Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest. It was a picture of a boa constrictor in the act of swallowing an animal. Here is a copy of the drawing. Boa In the book it said: Boa constric";
+    uint8_t rem_inBytes = CODE_LENGTH;
+    uint8_t rem_outBytes = OUT_SIZE_BITS;
 
-    unsigned int *test_output = (unsigned int *)malloc(8192*sizeof(unsigned int));
-    //string s = "The Little Prince Chapter I Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest. It was a picture of a boa constrictor in the act of swallowing an animal. Here is a copy of the drawing. Boa In the book it said: Boa constric";
-    unsigned char *input = (unsigned char *)malloc(300*sizeof(unsigned char));//"WYS*WYGWYS*WYSWYSG";  
-    string s = "WYS*WYGWYS*WYSWYSG";
-    //memcpy(input,"WYS*WYGWYS*WYSWYSG",s.size());
-    for(int i=0; i<s.size();i++){
-        input[i] = s[i];
-    }
+    for(int i=0; i<compressed_data.size(); i++){
+        int inData = compressed_data[i];
+        rem_inBytes = CODE_LENGTH;
 
-    
+        while(rem_inBytes){
 
-    //unsigned char HW_input[19] =  "WYS*WYGWYS*WYSWYSG";
+            int read_bits = MIN(rem_inBytes,rem_outBytes);
+            Byte = (Byte << read_bits) | ( inData >> (rem_inBytes - read_bits) );
 
-    //std::vector<int> encoding_out= encoding(s);
-    //std::vector<unsigned char> output_code = compress_test(encoding_out);
-    std::vector<int> output_code = encoding(s);
+            rem_inBytes -= read_bits;
+            rem_outBytes -= read_bits;
+            Length += read_bits;
+            inData &= ((0x1 << rem_inBytes) - 1);
 
-    printf("testbench sending input to hw\n");
-    for(int i=0; i<s.size(); i++){
-        printf("%c",input[i]);
-    }
-    unsigned int *HW_output_length = (unsigned int *)malloc(1*sizeof(unsigned int));
-    LZW_encoding_HW(input,s.size(),test_output, HW_output_length);
-    printf("\nChecking Starts\n");
-    int Equal = 1; 
-
-    if((*HW_output_length)!= output_code.size()){
-        Equal = 0;
-        printf("\nLengths of SW and HW not matching");
-        printf("\nSW length = %d\n",output_code.size());
-        printf("\nHW length = %d\n",*HW_output_length);
-    }
-    
-    for(int i = 0; i<output_code.size();i++){
-        if(output_code[i] != test_output[i]){
-            Equal = 0;
-            printf("mismatch at %d\n",i);
-            printf("SW value = %d\tHW value = %d\n",output_code[i],test_output[i]);
-            //break;
+            if(rem_outBytes == 0){
+                // file[offset + Length/8 - 1] = Byte;
+                output_vector.push_back(Byte); 
+                Byte = 0;
+                rem_outBytes = OUT_SIZE_BITS;
+            }
         }
     }
 
-    //printf("last_value SW = %d\tlast_value HW=%d\n",output_code[output_code.size()-1],test_output[HW_output_length-1]);
-    
+    if(Length % 8 > 0){
+        Byte = Byte << (8 - (Length%8));
+        Length += (8 - (Length%8));
+        // file[offset + Length/8 - 1] = Byte;
+        output_vector.push_back(Byte);
+    }
+
+    return output_vector;  // return number of bytes to be written to output file
+}
 
 
+bool compare_outputs(vector<unsigned char> sw_output_code, unsigned char *hw_output_code)
+{
+    bool Equal = true;
+    for (int i=0; i<sw_output_code.size(); i++)
+    {
+        if(sw_output_code[i] != hw_output_code[i+4 ])
+        {
+            cout<<"Element="<< i <<" SW out code: "<<sw_output_code[i]<<" HW out code: "<<hw_output_code[i]<<'\n';
+            Equal = false;
+        }
+    }
+    return Equal;
+}
+
+int main()
+{
+    // string test_string ="The Little Prince Chapter I Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest. It was a picture of a boa constrictor in the act of swallowing an animal. Here is a copy of the drawing. Boa In the book it said: Boa constric";
+    string test_string = "gdgserge yy66ey   &&**Ggg *GGGGGGGGGGGGGGabc gdgserge yy66ey   &&**Ggg *GGGGGGGGGGGGGGabc gdgserge yy66ey   &&**Ggg *GGGGGGGGGGGGGGabc gdgserge yy66ey   &&**Ggg *GGGGGGGGGGGGGGabc gdgserge yy66ey   &&**Ggg *GGGGGGGGGGGGGGabc gdgserge yy66ey   &&**Ggg *GGGGGGGGGGGGGGG";
+   
+    unsigned char* s_char = (uint8_t*)calloc(500,sizeof(uint8_t));
+    unsigned char* hw_output_code = (unsigned char*)calloc(8192, sizeof(unsigned char));
+
+    for(int i = 0; i < test_string.size(); i++)
+    {
+        s_char[i] = test_string[i];
+    }
+
+    uint32_t* output_code_size = (uint32_t*)calloc(1,sizeof(uint32_t));
+
+    LZW_encoding_HW(s_char,test_string.size(), hw_output_code, output_code_size);
+    vector<int> sw_output_code = LZW_SW(test_string);
+
+    std::vector<unsigned char> sw_out = compress(sw_output_code);
+
+    bool Equal = compare_outputs(sw_out, hw_output_code);
     std::cout << "TEST " << (Equal ? "PASSED" : "FAILED") << std::endl;
-
-
-    // free(test_output);
-    // free(input);
-    // free(HW_output_length);
-
-    return !Equal;
-
-    // cout << "Output Codes are: ";
-    // for (int i = 0; i < output_code.size(); i++) {
-    //     cout << output_code[i] << " ";
-    // }
-    // cout << endl;
-    // decoding(output_code);
 }
